8705bde86f09885eaeb654aef82f376d24e96db3
cfvalenzuela-vidal <cfvalenzuela.vidal@gmail.com>
2025-06-29 17:17:08 -0400

    23 commit, win

--- src/api.py
+++ src/api.py
@@ -27,5 +27,5 @@ def formulario_usuario():
 
     return render_template('formulario.html', usuarios=usuarios_info)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False)
