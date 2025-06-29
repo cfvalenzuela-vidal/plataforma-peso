acfb5633d139a2d43a73512194d560a9b15ab491
cfvalenzuela-vidal <cfvalenzuela.vidal@gmail.com>
2025-06-29 18:18:08 -0400

    44 commit, jmeter

--- src/api.py
+++ src/api.py
@@ -28,5 +28,5 @@ def formulario_usuario():
     return render_template('formulario.html', usuarios=usuarios_info)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False, threaded=True)
+    app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False, threaded=True)
 
